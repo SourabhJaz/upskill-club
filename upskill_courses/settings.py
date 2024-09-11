b9b116c5f9b872f805f7e9a45e59ef8cdcd7090c
SourabhJaz <sourabhjajoria@gmail.com>
2024-09-11 18:14:28 +0530

    Static folder for deployment

--- upskill_courses/settings.py
+++ upskill_courses/settings.py
@@ -168,6 +168,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # For production
+
 # Custom settings for media
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = 'media/'
