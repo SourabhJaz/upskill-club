5e6270e4c49e4fa61e61aa728462fa35cd0bcecf
SourabhJaz <sourabhjajoria@gmail.com>
2024-09-11 21:56:03 +0530

    Removing all secrets from code

--- upskill_courses/settings.py
+++ upskill_courses/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h=j=1g351@y6@xa(@-n@qb2brjg(#ia8@x-9$f$qrtefyk3d=n'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
