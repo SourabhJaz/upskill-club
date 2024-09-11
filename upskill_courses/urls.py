a97a53949d40f6e29044a784d71bb9ea1bb5f9e0
SourabhJaz <sourabhjajoria@gmail.com>
2024-09-11 14:34:13 +0530

    Adding config constants changes to gitignore

--- upskill_courses/urls.py
+++ upskill_courses/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from config.constants import ADMIN_URL
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(ADMIN_URL, admin.site.urls),
     path('api/', include('api.urls')),
 ]
