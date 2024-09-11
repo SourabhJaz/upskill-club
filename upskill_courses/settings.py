fb05139da02f6144408ed2516c937e7e77d42a64
SourabhJaz <sourabhjajoria@gmail.com>
2024-09-11 21:59:03 +0530

    Path changes

--- upskill_courses/settings.py
+++ upskill_courses/settings.py
@@ -168,7 +168,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # For production
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # For production
 
 # Custom settings for media
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
