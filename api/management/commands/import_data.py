e78296e80d5d376a20d65eb930c6e60397a4c6bc
SourabhJaz <sourabhjajoria@gmail.com>
2024-09-15 10:27:42 +0530

    Script to write models from JSON

--- api/management/commands/import_data.py
+++ api/management/commands/import_data.py
@@ -1,13 +1,33 @@
 import json
 from django.core.management.base import BaseCommand, CommandParser
-from api.models import *
+from api.models import Author, Session, Concept, Course, Category
 
 class Command(BaseCommand):
-    help = 'Imports data from JSON file'
+    help = 'Imports data from JSON file into a model'
     
     def add_arguments(self, parser: CommandParser):
-        parser.add_argument('json_file', type=str, help = 'Path to JSON file')
+        parser.add_argument('--json_file', type=str, help = 'Path to JSON file')
+        parser.add_argument('--model_name', type=str, help = 'Model name')
         
     def handle(self, *args, **options):
         json_file = options['json_file']
-        self.stdout.write(self.style.SUCCESS('Ran Command'))
+        model_name = options['model_name']
+        if model_name == 'Author':
+            selected_model = Author
+        elif model_name == 'Session':
+            selected_model = Session
+        elif model_name == 'Course':
+            selected_model = Course
+        elif model_name == 'Category':
+            selected_model = Category
+        elif model_name == 'Concept':
+            selected_model = Concept
+        else:
+            self.stderr.write(self.style.ERROR(f'Unknown model type {model_name}'))
+            return
+        with open(json_file, 'r') as file:
+            data = json.load(file)
+            for item in data:
+                self.stdout.write(self.style.NOTICE(f'Running Command for {model_name} {item}'))
+                (object, created) = selected_model.objects.update_or_create(id = item['id'], defaults = item)            
+                self.stdout.write(self.style.SUCCESS(f'Command output {object} {created}'))
